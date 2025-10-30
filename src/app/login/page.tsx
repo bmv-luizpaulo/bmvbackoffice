a50749df9b1c5ffc4d504e236ce3d56d1f1c57c6
Devluizpaulo <136992113+Devluizpaulo@users.noreply.github.com>
2025-10-30 08:24:35 -0300

    sdds

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -9,7 +9,7 @@ export default function LoginPage() {
   return (
     <FirebaseClientProvider>
       <main className="relative flex min-h-screen items-center justify-center p-4">
-        <Image 
+        <Image
           src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzODE4MDB8MHwxfGFsbHx8fHx8fHx8fDE3MjM1ODk5MDZ8&ixlib=rb-4.0.3&q=80&w=1080"
           alt="Escritório moderno"
           fill
@@ -20,10 +20,10 @@ export default function LoginPage() {
         <div className="w-full max-w-5xl">
           <Card className="w-full bg-background/90 backdrop-blur-sm">
             <div className="grid items-stretch md:grid-cols-2">
-              <CardHeader className="p-8 md:p-10 border-b md:border-b-0 md:border-r border-border">
-                <div className="flex items-center gap-4 md:gap-6 justify-center md:justify-start">
-                  <Image src="/image/BMV.png" alt="BMV Logo" width={64} height={64} className="rounded-md" />
-                  <h1 className="font-headline text-4xl md:text-5xl font-bold tracking-tight text-black">Nexus</h1>
+              <CardHeader className="p-10 md:p-10 border-b md:border-b-0 md:border-r border-border">
+                <div className="flex items-center gap-4 md:gap-0 justify-center md:justify-center">
+                  <Image src="/image/BMV.png" alt="BMV Logo" width={200} height={204} className="rounded-md" />
+                  <h1 className="font-headline text-5xl md:text-4xl font-bold tracking-tight text-black">Nexus</h1>
                 </div>
               </CardHeader>
               <CardContent className="p-8 md:p-10">
