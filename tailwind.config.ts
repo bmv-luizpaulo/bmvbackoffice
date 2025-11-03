5caf53be0b61fc21b50c06c7cbf8fb6fb0801471
Luiz Paulo Gonçalves Miguel de Jesus <luizpaulo.jesus@bmv.global>
2025-11-03 17:11:02 +0000

    use essa fonte no nosso projeto

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['"PT Sans"', 'sans-serif'],
-        headline: ['"Playfair Display"', 'serif'],
+        headline: ['"Cormorant Garamond"', 'serif'],
         code: ['monospace'],
       },
       colors: {
