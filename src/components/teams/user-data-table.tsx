8cb385336561abd54309f6a62cf7d2ec303f3799
Luiz Paulo Gonçalves Miguel de Jesus <luizpaulo.jesus@bmv.global>
2025-11-12 18:59:35 +0000

    Try fixing this error: `Runtime Error: Error: Label is not defined. Erro

--- src/components/teams/user-data-table.tsx
+++ src/components/teams/user-data-table.tsx
@@ -33,6 +33,7 @@ import {
 } from "@/components/ui/table"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { Checkbox } from "@/components/ui/checkbox"
 import type { User, Role } from "@/lib/types";
