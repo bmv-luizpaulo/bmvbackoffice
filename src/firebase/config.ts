f79fc58fd335825bce4f466b337e4e61563d84ac
Luiz Paulo Gonçalves Miguel de Jesus <luizpaulo.jesus@bmv.global>
2025-12-19 11:47:48 +0000

    revise // Import the functions you need from the SDKs you need import {

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-4461945520-252d9",
-  "appId": "1:638509620097:web:9e95877a01762c9931e680",
   "apiKey": "AIzaSyDJVyIidl27RcEz2DwbpwGVZeHeMLewiNo",
   "authDomain": "studio-4461945520-252d9.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "638509620097"
+  "projectId": "studio-4461945520-252d9",
+  "storageBucket": "studio-4461945520-252d9.appspot.com",
+  "messagingSenderId": "638509620097",
+  "appId": "1:638509620097:web:9e95877a01762c9931e680",
+  "measurementId": "G-XXXXXXXXXX"
 };
